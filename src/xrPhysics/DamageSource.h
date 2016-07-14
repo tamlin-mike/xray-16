a15c2524d0c6523943405112f0ce725157889d05
tamlin-mike <tamlin@wurmonline.com>
2016-07-14 06:35:12 +0200

    More PCH work for xrGame.

--- src/xrPhysics/DamageSource.h
+++ src/xrPhysics/DamageSource.h
@@ -1,11 +1,13 @@
 #pragma once
  
+#include "xrCore/xrDebug_macros.h" // ONLY for pragma todo!
+#pragma todo("__declspec(novtable) for IDamageSource ?")
 
 class IDamageSource
 {
 public:
-	virtual							~IDamageSource			()												{}				;
-	virtual			void			SetInitiator			(u16 id)										=0				;
-	virtual			u16				Initiator				()												=0				;
-	virtual			IDamageSource	*cast_IDamageSource		()												=0				;//{return this	;}
+	virtual						~IDamageSource() { };
+	virtual		void			SetInitiator(u16 id) = 0;
+	virtual		u16				Initiator() = 0;
+	virtual		IDamageSource	*cast_IDamageSource() = 0;//{return this	;}
 };
